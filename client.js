8f11f4eea41e37ad38a8e882240fe19a409c42b6
Choigmaw <tjcsm0320@gmail.com>
2024-07-04 00:20:37 +0900

    feat: 일정간격 전송 구현

--- client.js
+++ client.js
@@ -1,8 +1,14 @@
 const dgram = require('dgram');
-
 const client = dgram.createSocket('udp4');
-
 const message = Buffer.from('Hello, UDP server');
-client.send(message, 41234, 'localhost', (err) => {
-    client.close();
-});
+
+// setInterval을 사용해서 일정 간격마다 전송
+setInterval(() => {
+    client.send(message, 41234, 'localhost', (err) => {
+        if (err) {
+            console.error('Error sending message : ', err);
+        } else {
+            console.log('Message sent to server');
+        }
+    });
+}, 1000);
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const server = dgram.createSocket('udp4');
 server.on('message', (msg, rinfo) => {
     // 템플릿 리터럴을 사용하려면 백틱으로 사용해야함, 작은 따옴표가 아님!
     console.log(`Server received: ${msg} from ${rinfo.address}:${rinfo.port}`);
+
 });
 
 server.bind(41234)
