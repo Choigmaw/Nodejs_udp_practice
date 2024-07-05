5b4a26b24efc987ebde1c36609d0a277a4d232f0
tjcsm <tjcsm0320@allnewsystems.com>
2024-07-05 13:20:16 +0900

    모듈설정 + DB 연결

--- db.js
+++ db.js
@@ -1,20 +1,22 @@
 const mysql = require('mysql2');
 const dotenv = require('dotenv');
+dotenv.config();
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'choigmaw0320',
-    database: 'test'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 });
 
 function connectToDatabase(callback) {
     
-    connection.connect((err) => {
+    pool.getConnection((err, connection) => {
         if (err) {
-            console.error('Error connecting to database:', err);
+            console.error('Error getting connection from pool:', err);
             if (callback) callback(err);
-            return;
+            return 
         }
         console.log('Connected to the database');
         if (callback) callback(null, connection);
